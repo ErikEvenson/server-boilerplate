9f9b44dd7ed889fa37365f7a968784a1aae25a6c
Erik Evenson <erik.e.evenson@gmail.com>
2015-06-25 21:23:14 -0500

    checkpoint

--- src/app/config/mailer.js
+++ src/app/config/mailer.js
@@ -1,7 +1,19 @@
 var
   mailer = require('express-mailer'),
-  mailerOptions = require('./secrets').mailerOptions;
+  secrets = require('./secrets');
 
 module.exports = function(app) {
+  var mailerOptions = {
+    auth: {
+      user: secrets.emailUsername,
+      pass: secrets.emailPassword
+    },
+    from: 'noreply@3eenterprises.com',
+    host: 'vps2.erikevenson.net',
+    secureConnection: true,
+    port: 465,
+    transportMethod: 'SMTP'
+  };
+
   mailer.extend(app, mailerOptions);
 };
